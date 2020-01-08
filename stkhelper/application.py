04c338054ddcdd051bc52d70e698cf8ea87e44f7
Conor McFerren <cnmcferren@gmail.com>
2020-01-08 18:55:35 -0500

    Written

---
+++ stkhelper/application.py
@@ -0,0 +1,24 @@
+"""
+
+Application class that creates an instance of the STK11 Application
+
+"""
+
+from win32api import GetSystemMetrics
+from comtypes.client import CreateObject
+
+
+class Application:
+    def __init__(self):
+    
+        #TODO Add __uiApplication to UML Diagram
+        self.__uiApplication = CreateObject("STK11.Application")
+        self.__uiApplication.Visible = True #Make graphics visible
+        self.__uiApplication.UserControl = True #Enable user control
+        self.root = self.__uiApplication.Personality2
+        
+    def Connect(self,connectCommand):
+        self.root.ExecuteCommand(connectCommand)
+        
+    def Close():
+        self.__uiApplication.Close()
---
+++ stkhelper/scenario.py
@@ -0,0 +1,54 @@
+from comtypes.gen import STKObjects
+from comtypes import COMError
+
+class Scenario:
+    def __init__(self, application, name, timePeriod, startTime=None):
+        """
+        Creates an instance of the scenario.
+        Parameters:
+            application (STK11 Instance): Running instance of STK 11.
+            name (str): Name of the scenario to be created.
+            timePeriod (str): The amount of time you want the scenario to run 
+            for.
+            startTime (str): The start date of the scenario.
+                        Examples: "+24hr", "+365days", "+10days".
+        """
+        
+        
+        if startTime == None:
+            startTime = 'Today'
+        else:
+            startTime = str(startTime)
+        
+        self.startTime = startTime
+        self.stopTime = timePeriod
+        
+        self.guardian = application
+        self.name = name.replace(' ','_')
+        
+        application.root.NewScenario(self.name)
+        self.reference = application.root.CurrentScenario
+        self.reference = self.reference.QueryInterface(STKObjects.IAgScenario)
+        try:
+            self.reference.SetTimePeriod(str(startTime),str(timePeriod))
+        except COMError:
+            raise ValueError("Time period not properly formatted")
+            
+    def SetTimePeriod(self, startTime, stopTime):    
+        """
+        Sets a new time period for the scenario.
+        Parameters:
+            elapsedTime (str): The amound of time you want the scenario to run 
+            for. Examples: "+24hr", "+365days", "+10days".
+        """
+
+        self.startTime = startTime
+        self.stopTime = stopTime
+        self.reference.SetTimePeriod(str(startTime),str(stopTime))
+        
+    def Close(self):     
+        """
+        Closes the scenario.
+        """
+
+        self.guardian.root.CloseScenario()
