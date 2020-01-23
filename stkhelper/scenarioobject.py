41da84725f8e8d35c61ca106573c5c3e0052833f
Conor McFerren <cnmcferren@gmail.com>
2020-01-23 11:35:50 -0500

    Added GetAccess

--- stkhelper/scenarioobject.py
+++ stkhelper/scenarioobject.py
@@ -6,3 +6,19 @@ class ScenarioObject:
     def __init__(self, guardian, name):
         self.guardian = guardian
         self.name = name
+        
+    def GetAccess(self,scenarioObject):
+        access = self.reference.GetAccessToObject(scenarioObject.reference)
+        access.ComputeAccess()
+        intervalCollection = access.ComputedAccessIntervalTimes
+        
+        try:
+            computedIntervals = intervalCollection.ToArray(0,-1)
+            self.root.EndUpdate()
+
+            return computedIntervals
+        
+        except Exception:
+            self.root.EndUpdate()
+
+            return 0
