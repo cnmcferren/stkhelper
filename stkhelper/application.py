c6d07c8634bbce50a32b1fb52c698c0282c78c5f
Conor McFerren <cnmcferren@gmail.com>
2020-01-28 12:30:08 -0500

    Comments added

--- stkhelper/application.py
+++ stkhelper/application.py
@@ -1,24 +1,35 @@
-"""
-
-Application class that creates an instance of the STK11 Application
-
-"""
-
 from win32api import GetSystemMetrics
 from comtypes.client import CreateObject
 
 
 class Application:
-    def __init__(self):
     
-        #TODO Add __uiApplication to UML Diagram
+    """
+    
+    Creates the application for STK
+    
+    """
+    def __init__(self):
         self.__uiApplication = CreateObject("STK11.Application")
         self.__uiApplication.Visible = True #Make graphics visible
         self.__uiApplication.UserControl = True #Enable user control
         self.root = self.__uiApplication.Personality2
         
+    """
+    
+    Used to run STK Connect Commands using the root
+    
+    Parameters:
+        connectCommand(str): The Connect Command string to be executed
+    
+    """
     def Connect(self,connectCommand):
         self.root.ExecuteCommand(connectCommand)
         
+    """
+    
+    Closes the application
+    
+    """
     def Close(self):
         self.__uiApplication.Close()
