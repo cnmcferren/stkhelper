18dc2e9bd5f808af22af3a9ae4490e605e301aff
W. Conor McFerren <cnmcferren@gmail.com>
2020-10-07 22:52:43 -0400

    Increment to 3.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ info = open('README.md').read()
 
 setup(
       name='stkhelper',
-      version='3.0.2',
+      version='3.0.3',
       description='Uses the STK software to create simulations for satellite testing',
       long_description=info,
       long_description_content_type='text/markdown',
