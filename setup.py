bb1cf1597bbea9ee4f2b1cccf316ba49e0f19334
W. Conor McFerren <cnmcferren@gmail.com>
2020-10-17 16:39:54 -0400

    Version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ info = open('README.md').read()
 
 setup(
       name='stkhelper',
-      version='3.0.3',
+      version='3.0.4',
       description='Uses the STK software to create simulations for satellite testing',
       long_description=info,
       long_description_content_type='text/markdown',
